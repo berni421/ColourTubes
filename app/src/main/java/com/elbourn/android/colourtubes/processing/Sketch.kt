cdd5e1fabd4d49f64ea582cf58f2fc8341631b41
Bernie Elbourn <benri421@gmail.com>
2023-02-04 18:13:49 +0000

    v0.0.2 origin moves with touches

--- app/src/main/java/com/elbourn/android/colourtubes/processing/Sketch.kt
+++ app/src/main/java/com/elbourn/android/colourtubes/processing/Sketch.kt
@@ -2,6 +2,7 @@ package com.elbourn.android.colourtubes.processing
 
 import android.util.Log
 import processing.core.PApplet
+import processing.core.PVector
 
 class Sketch : PApplet() {
     val TAG: String = javaClass.simpleName
@@ -13,6 +14,8 @@ class Sketch : PApplet() {
         size(displayWidth, displayHeight, P3D)
     }
 
+    var oX = 0
+    var oY = 0
     override fun setup() {
         Log.i(TAG, "start setup")
         // Check permissions
@@ -22,19 +25,19 @@ class Sketch : PApplet() {
         tubes = Tubes(this)
         background(0)
         frameRate(10f)
+        oX = width/2
+        oY = height/2
         Log.i(TAG, "end setup")
     }
 
     override fun draw() {
-//        Log.i(TAG, "started draw")
         // Check permissions OK
         if (!hasPermissions(permissions)) return
         // Display current tubes state
         background(0)
-        translate(width/2f, height/2f, 0f)
+        translate(oX.toFloat(), oY.toFloat(), 0f)
         rotateY(0.1f * frameCount / TWO_PI)
         tubes.display()
-//        noLoop()
     }
 
     private fun requestPermissions(permissions: Array<String>?) {
@@ -58,9 +61,13 @@ class Sketch : PApplet() {
     }
 
     override fun touchStarted() {
+        oX = mouseX
+        oY = mouseY
     }
 
     override fun touchMoved() {
+        oX = mouseX
+        oY = mouseY
     }
 
     override fun touchEnded() {
